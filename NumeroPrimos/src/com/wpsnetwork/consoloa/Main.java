64ed154edc61094435379f0f7620b953ebad2218
javier juan <jjberciano1975@gmail.com>
2016-01-25 20:24:13 +0100

    metidos log4j2

--- NumeroPrimos/src/com/wpsnetwork/consoloa/Main.java
+++ NumeroPrimos/src/com/wpsnetwork/consoloa/Main.java
@@ -1,8 +1,27 @@
 package com.wpsnetwork.consoloa;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
 
+	
+	private static final Logger logger = LogManager.getLogger(Main.class.getName());
+	
+
+		
+	
+	
+
+	
 	public static void main(String[] args) {
+		
+		 //Escribir varios mensajes
+		logger.trace("Este es un mensaje de trace");
+		logger.debug("Este es un mensaje de debug");
+		logger.info("Este es un mensaje de info");
+		logger.warn("Este es un mensaje de warning");
+		logger.error("Este es un mensaje de error");
+		logger.fatal("Este es un mensaje fatal");
+		
 		// TODO Auto-generated method stub
      System.out.println(calculaPrimo(20,22));
 	}
@@ -32,6 +51,13 @@ public class Main {
 				}            
 			}
 		while (!esPrimo && primo<maximo);            
-		return esPrimo? primo : 0;          
+		return esPrimo? primo : 0;  
+		
+		
+		
+
 		} 
+	
+	
+	
 }
